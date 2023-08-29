61c7a2e433552c08c3656c7067cc819d6670cad4
mauricioroza <mauricio_roza@msn.com>
2023-08-29 23:50:28 +0200

    adjusted circular plot to show different colors and size to selected genes, and function to create .svg

--- code/circlize.R
+++ code/circlize.R
@@ -1,18 +1,28 @@
 library(circlize)
-
-brain.meth.dir <- c("../data/myDiff.brain.RData")
+library(tidyverse)
+library(svglite)
+  
+brain.meth.dir <- c("./data/myDiff.brain.RData")
 load(brain.meth.dir)
 
-
+myDiff10p.brain <- getMethylDiff(myDiff.brain,
+                                   difference=meth.cut,
+                                   qvalue=qvalue.cut)
+myDiff10p.brain <- myDiff10p.brain[order(myDiff10p.brain$qvalue),]
+  
 myDiff10p.hyper.brain <- getMethylDiff(myDiff.brain,
-                                 difference=meth.cut,
-                                 qvalue=qvalue.cut,
-                                 type = "hyper")
-
+                                         difference=meth.cut,
+                                         qvalue=qvalue.cut,
+                                         type = "hyper")
+  
 myDiff10p.hypo.brain <- getMethylDiff(myDiff.brain,
-                                       difference=meth.cut,
-                                       qvalue=qvalue.cut,
-                                       type = "hypo")
+                                        difference=meth.cut,
+                                        qvalue=qvalue.cut,
+                                        type = "hypo")
+  
+t.brain <- readRDS("./data/t.brain.rds")
+  
+
 
 DMR_hyper <- data.frame("chr" = myDiff10p.hyper.brain$chr,
                         "start" = myDiff10p.hyper.brain$start,
@@ -43,42 +53,48 @@ DMR_all <- DMR_all %>%
     meth.diff < 10 & meth.diff > -10 ~ "#D3D3D3"
   )) 
 
-DMR_all$loc <- paste(DMR_all$chr, DMR_all$start, DMR_all$end, sep = "_")
 
+
+rlv.genes.brain <- c("grik2", "slc17a7", "grm4", "grm5", "grin1", "grm5", "grm1", "grm8", "grin2b", #glutamate signaling
+                     "gabbr1", "gabbr2", "gabrb3", #GABA signaling
+                     "dnmt3a", "hdac8", "mbd2", #methylation
+                     "gh1", "irs2", "igfbp4", "igfbp5", #somatotropic
+                     "trhr3", "trhde", "dio1", "\\btg\\b", #thyrotropic 
+                     "esrrg",
+                     "kiss2", "prkag2", "prkar1b", #GnRH signaling
+                     "hsd17b12") #steroidogenesis
+
+hyper_color_rlv <- "#660000"
+hypo_color_rlv <- "#000166"
 
 annot <- t.brain %>% 
   filter(str_detect(external_gene_name, paste(rlv.genes.brain, collapse = "|"))) %>%
   tidyr::unite("loc", c("seqnames", "start", "end"), remove = FALSE) %>%
   dplyr::select(seqnames, start, end, mcols.meth.diff, external_gene_name, loc) %>%
-  mutate(color = case_when(
-    mcols.meth.diff >= 10 ~ "#660000",
-    mcols.meth.diff <= -10 ~ "#000166"
-  ),
-  pointcolor = "green"
+  mutate(pointcolor = case_when(
+    mcols.meth.diff >= 10 ~ hyper_color_rlv,
+    mcols.meth.diff <= -10 ~ hypo_color_rlv
+  )
   )
 
 pointcolor <- annot %>% dplyr::select(pointcolor, loc)
 
-DMR_all_colorcex <- left_join(DMR_all, pointcolor, by = "loc") %>%
+DMR_all2 <- DMR_all
+DMR_all2$loc <- paste(DMR_all$chr, DMR_all$start, DMR_all$end, sep = "_")
+
+DMR_all2 <- left_join(DMR_all2, pointcolor, by = "loc") %>%
   mutate(color = if_else(is.na(pointcolor), color, pointcolor),
          cex = case_when(
-           color == "green" ~ 1,
+           color == hyper_color_rlv ~ 1,
+           color == hypo_color_rlv ~ 1,
            TRUE ~ 0.5
          )) %>%
   dplyr::select(-loc, -pointcolor)
 
-
-tiff("./figures/circos_brain.tiff", 
-     width = 2000, 
-     height = 2000, 
-     units = "px", 
-     pointsize = 12,
-     compression = "none",
-     bg = "white",
-     res = 300
-) 
+DMR_all2 <- DMR_all2[order(DMR_all2$cex),]
 
 
+circos.brain <- function() {
 circos.clear()
 
 circos.par(gap.after = c(rep(2, 9), 7), start.degree = 90)
@@ -89,16 +105,16 @@ circos.initializeWithIdeogram(plotType = c("labels", "axis"),
 circos.par("track.height" = 0.3)
 
 
-circos.genomicTrack(DMR_all, 
+circos.genomicTrack(DMR_all2[,1:4], 
                     stack = FALSE, 
                     ylim = c(-100, 100),
                     panel.fun = function(region, value, ...) {
-                      cex = DMR_all_colorcex[rownames(value), 6]
-                      i = DMR_all_colorcex[rownames(value), 5]
+                      cex = DMR_all2[rownames(value), "cex"]
+                      i = DMR_all2[rownames(value), "color"]
                       for(h in seq(-100, 100, by = 25)) {
                         circos.lines(CELL_META$cell.xlim, c(h, h), lty = 3, col = "#E7E7E7")
                       }
-                      circos.genomicPoints(region, value, cex = cex, pch = 16, col = i, ...)
+                      circos.genomicPoints(region, value, cex = cex, pch = 16, col = i, bg = i, ...)
                       circos.lines(CELL_META$cell.xlim, c(-10, -10), col = "#7D7D7D", lwd = 1, lty = "dashed")
                       circos.lines(CELL_META$cell.xlim, c(10, 10), col = "#7D7D7D", lwd = 1, lty = "dashed")
                       circos.lines(CELL_META$cell.xlim, c(0, 0), col = "#888888", lwd = 1, lty = "dotted")
@@ -109,10 +125,32 @@ circos.genomicTrack(DMR_all,
                                    labels.cex = 0.3
                       )
                     })  
-circos.genomicLabels(annot, labels.column = 5, cex = 0.8, col= annot$color, line_lwd = 1, line_col="grey10", 
+circos.genomicLabels(annot, labels.column = 5, cex = 0.8, col= annot$pointcolor, line_lwd = 1, line_col="grey10", 
                                            side="inside", connection_height=0.05, labels_height=0.04)
+
+}
+tiff("./figures/circos_brain.tiff", 
+     width = 2000, 
+     height = 2000, 
+     units = "px", 
+     pointsize = 12,
+     compression = "none",
+     bg = "white",
+     res = 300
+) 
+
+circos.brain()
+
 dev.off()
 
+svglite("./figures/circos_brain.svg",
+        width = 15,
+        height = 15,
+        scaling = 2.2)
+
+circos.brain()
+
+dev.off()
 
 
 
